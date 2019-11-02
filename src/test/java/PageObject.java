9d0b57c1bd6c5f7d4d607ae4e53b9a34bf5049d6
Flowmotions <vasilijf97@gmail.com>
2019-11-02 14:34:58 +0300

    changes2

--- src/test/java/PageObject.java
+++ src/test/java/PageObject.java
@@ -3,7 +3,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -57,7 +56,6 @@ public class PageObject {
         authBtn.click();
 
         waitFor.until(ExpectedConditions.visibilityOf(authIcon));
-
     }
 
     void logout() {
